d4a3b52e93e9ff06baad5c852d16f9a62bdc03b2
hattoriyoshiki <hykprogram@gmail.com>
2024-10-23 17:26:34 +0900

    [WIP]hotfix入力項目追加部分のバグ修正
    コードが間違っていたため修正しました。

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -19,7 +19,7 @@
         <!-- phone -->
         <div>
             <x-input-label for="phone" :value="__('Phone')" />
-            <x-text-input id="phone" class="block mt-1 w-full" type="text" name="phone" :value="old('prefecture')" required autofocus autocomplete="phone" />
+            <x-text-input id="phone" class="block mt-1 w-full" type="text" name="phone" :value="old('phone')" required autofocus autocomplete="phone" />
             <x-input-error :messages="$errors->get('phone')" class="mt-2" />
         </div>
 
