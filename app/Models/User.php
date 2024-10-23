cecca0b1f9270ee987844e410e1c81508fefd727
hattoriyoshiki <hykprogram@gmail.com>
2024-10-23 14:04:46 +0900

    [WIP]disableメール送信機能の一時停止
    ログイン後のダッシュボードにユーザー情報を表示する画面を作るために一旦停止した。

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,13 +2,13 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
+//後で実装use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable implements MustVerifyEmail
+class User extends Authenticatable //後で実装implements MustVerifyEmail
 {
     use HasApiTokens, HasFactory, Notifiable;
 
