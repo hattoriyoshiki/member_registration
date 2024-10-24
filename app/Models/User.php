0ea1497d36461ea9f276d76f93334020ad21bba6
hattoriyoshiki <hykprogram@gmail.com>
2024-10-24 22:10:14 +0900

    [WIP]fix_一時的に停止していたメール送信機能
    メール送信機能を再実装しました。http://localhost:8025で確認済み。

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,13 +2,13 @@
 
 namespace App\Models;
 
-//後で実装use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable //後で実装implements MustVerifyEmail
+class User extends Authenticatable implements MustVerifyEmail
 {
     use HasApiTokens, HasFactory, Notifiable;
 
